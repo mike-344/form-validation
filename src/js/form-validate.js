d990e144855f19d60b1e8b6211c11513d4f22cad
Michael Sullivan <sullivanmichael344@gmail.com>
2024-08-08 00:10:24 -0400

    add zip error handling

--- src/js/form-validate.js
+++ src/js/form-validate.js
@@ -7,4 +7,32 @@ function formValidateHandler(){
     const submit = document.querySelector(".submit");
 
     
+    submit.addEventListener("click",(e) =>{
+        e.preventDefault();
+    })
+    
+    email.addEventListener("input", (event) => {
+         if (email.validity.typeMismatch) {
+             email.setCustomValidity("Must be a valid email!");
+         } else {
+            email.setCustomValidity("");
+         }
+         email.reportValidity();
+          });
+    
+    zip.addEventListener("input", (event) =>{
+        if (zip.validity.tooShort || zip.validity.tooLong){
+            zip.setCustomValidity("Must be 5 digits!");
+            zip.reportValidity();
+        } else if (zip.validity.patternMismatch) {
+            zip.setCustomValidity("Cannot contain letters!");
+            zip.reportValidity();
+        } else{
+            zip.setCustomValidity("");
+        }
+    })
+
+
 }
+
+export {formValidateHandler}
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 import '../css/style.css'
+import { formValidateHandler } from './form-validate'
 
+formValidateHandler();
