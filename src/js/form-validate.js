5886dcd74d44fb175d3efb95cd03f1b8794d9f83
Michael Sullivan <sullivanmichael344@gmail.com>
2024-08-07 23:25:46 -0400

    Set outline style

---
+++ src/js/form-validate.js
@@ -0,0 +1,10 @@
+function formValidateHandler(){
+    const email = document.querySelector("#email");
+    const country = document.querySelector("#country");
+    const zip = document.querySelector("#zip");
+    const password = document.querySelector("#password");
+    const passwordConfirm = document.querySelector("#password-confirm");
+    const submit = document.querySelector(".submit");
+
+    
+}
